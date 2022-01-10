749bec53369596aab2beb2cedf98b41316e40b0b
Gesa Stupperich <gesa.stupperich@gmail.com>
2022-01-10 20:58:16 +0000

    Determine delta in conditionals and update outside of conditionals

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -9,6 +9,7 @@ class GildedRose {
 
     public void updateQuality() {
         for (Item item : items) {
+            int delta;
             switch (item.name) {
                 case "Sulfuras, Hand of Ragnaros":
                     break;
@@ -16,37 +17,35 @@ class GildedRose {
                 case "Aged Brie":
                     item.updateSellIn(-1);
 
+                    delta = 1;
                     if (item.sellIn < 0) {
-                        item.updateQuality(2);
-                    } else {
-                        item.updateQuality(1);
+                        delta = 2;
                     }
+                    item.updateQuality(delta);
                     break;
 
                 case "Backstage passes to a TAFKAL80ETC concert":
                     item.updateSellIn(-1);
 
-                    if (item.sellIn < 0) {
-                        item.quality = 0;
-                    } else {
-                        if (item.sellIn >= 10) {
-                            item.updateQuality(1);
-                        } else if (item.sellIn >= 5) {
-                            item.updateQuality(2);
-                        } else if (item.sellIn >= 0) {
-                            item.updateQuality(3);
-                        }
+                    delta = -item.quality;
+                    if (item.sellIn >= 10) {
+                        delta = 1;
+                    } else if (item.sellIn >= 5) {
+                        delta = 2;
+                    } else if (item.sellIn >= 0) {
+                        delta = 3;
                     }
+                    item.updateQuality(delta);
                     break;
 
                 default:
                     item.updateSellIn(-1);
 
+                    delta = -1;
                     if (item.sellIn < 0) {
-                        item.updateQuality(-2);
-                    } else {
-                        item.updateQuality(-1);
+                        delta = -2;
                     }
+                    item.updateQuality(delta);
                     break;
             }
         }
