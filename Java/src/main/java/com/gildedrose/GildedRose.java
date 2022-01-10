e95cb5315c26eeb70ff9338bf78d36bf7ee506e4
Gesa Stupperich <gesa.stupperich@gmail.com>
2022-01-10 21:21:06 +0000

    Extract quality delta decision into function

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -12,39 +12,34 @@ class GildedRose {
             if (!"Sulfuras, Hand of Ragnaros".equals(item.name)) {
                 item.updateSellIn(-1);
             }
-
-            int updateDelta;
-            switch (item.name) {
-                case "Sulfuras, Hand of Ragnaros":
-                    updateDelta = 0;
-                    break;
-
-                case "Aged Brie":
-                    updateDelta = 2;
-                    if (item.sellIn >= 0) {
-                        updateDelta = 1;
-                    }
-                    break;
-
-                case "Backstage passes to a TAFKAL80ETC concert":
-                    updateDelta = -item.quality;
-                    if (item.sellIn >= 10) {
-                        updateDelta = 1;
-                    } else if (item.sellIn >= 5) {
-                        updateDelta = 2;
-                    } else if (item.sellIn >= 0) {
-                        updateDelta = 3;
-                    }
-                    break;
-
-                default:
-                    updateDelta = -2;
-                    if (item.sellIn >= 0) {
-                        updateDelta = -1;
-                    }
-                    break;
-            }
-            item.updateQuality(updateDelta);
+            int qualityDelta = determineQualityDelta(item);
+            item.updateQuality(qualityDelta);
         }
+    }
+
+    private static int determineQualityDelta(Item item) {
+        switch (item.name) {
+            case "Sulfuras, Hand of Ragnaros":
+                return 0;
+            case "Aged Brie":
+                return item.sellIn >= 0 ? 1 : 2;
+            case "Backstage passes to a TAFKAL80ETC concert":
+                return determineBackstageUpdateDelta(item);
+            default:
+                return item.sellIn >= 0 ? -1 : -2;
+        }
+    }
+
+    private static int determineBackstageUpdateDelta(Item item) {
+        assert item.name.equals("Backstage passes to a TAFKAL80ETC concert");
+        int updateDelta = -item.quality;
+        if (item.sellIn >= 10) {
+            updateDelta = 1;
+        } else if (item.sellIn >= 5) {
+            updateDelta = 2;
+        } else if (item.sellIn >= 0) {
+            updateDelta = 3;
+        }
+        return updateDelta;
     }
 }
