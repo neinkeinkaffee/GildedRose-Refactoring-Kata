309679f95293b6a58d38e6b6b39d95b4b2782813
Gesa Stupperich <gesa.stupperich@gmail.com>
2022-01-10 20:29:07 +0000

    Always start with sellIn < 0 case

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -16,34 +16,36 @@ class GildedRose {
                 case "Aged Brie":
                     item.updateSellIn(-1);
 
-                    if (item.sellIn >= 0) {
-                        item.updateQuality(1);
-                    } else {
+                    if (item.sellIn < 0) {
                         item.updateQuality(2);
+                    } else {
+                        item.updateQuality(1);
                     }
                     break;
 
                 case "Backstage passes to a TAFKAL80ETC concert":
                     item.updateSellIn(-1);
 
-                    if (item.sellIn >= 10) {
-                        item.updateQuality(1);
-                    } else if (item.sellIn >= 5) {
-                        item.updateQuality(2);
-                    } else if (item.sellIn >= 0) {
-                        item.updateQuality(3);
-                    } else {
+                    if (item.sellIn < 0) {
                         item.quality = 0;
+                    } else {
+                        if (item.sellIn >= 10) {
+                            item.updateQuality(1);
+                        } else if (item.sellIn >= 5) {
+                            item.updateQuality(2);
+                        } else if (item.sellIn >= 0) {
+                            item.updateQuality(3);
+                        }
                     }
                     break;
 
                 default:
                     item.updateSellIn(-1);
 
-                    if (item.sellIn >= 0) {
-                        item.updateQuality(-1);
-                    } else {
+                    if (item.sellIn < 0) {
                         item.updateQuality(-2);
+                    } else {
+                        item.updateQuality(-1);
                     }
                     break;
             }
