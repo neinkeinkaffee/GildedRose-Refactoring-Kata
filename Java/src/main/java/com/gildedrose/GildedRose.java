2687e20a18a00e3ef9ddc67e9c802b383d967fd6
Gesa Stupperich <gesa.stupperich@gmail.com>
2022-01-11 15:44:21 +0000

    Reintroduce policy/strategy class

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -9,57 +9,89 @@ class GildedRose {
 
     public void updateQuality() {
         for (Item item : items) {
-            updateSellIn(item);
-            updateQuality(item);
+            updateOneItem(item);
         }
     }
 
-    private void updateSellIn(Item item) {
-        switch (item.name) {
-            case "Sulfuras, Hand of Ragnaros":
-                return;
-        }
-        item.sellIn -= 1;
+    private void updateOneItem(Item item) {
+        ItemUpdateStrategy strategy = determineUpdateStrategy(item);
+        strategy.updateSellIn(item);
+        strategy.updateQuality(item);
     }
 
-    private void updateQuality(Item item) {
+    private ItemUpdateStrategy determineUpdateStrategy(Item item) {
         switch (item.name) {
             case "Sulfuras, Hand of Ragnaros":
-                break;
+                return new LegendaryStrategy();
             case "Backstage passes to a TAFKAL80ETC concert":
-                if (item.sellIn < 0) {
-                    item.quality = 0;
-                } else {
-                    if (item.sellIn < 5) {
-                        incrementQuality(item);
-                    }
-                    if (item.sellIn < 10) {
-                        incrementQuality(item);
-                    }
-                    incrementQuality(item);
-                }
-                break;
+                return new BackstagePassStrategy();
             case "Aged Brie":
-                incrementQuality(item);
-                if (item.sellIn < 0) {
+                return new BrieStrategy();
+            case "Conjured Mana Cake":
+                return new ConjuredStrategy();
+        }
+        return new ItemUpdateStrategy();
+    }
+
+    private class LegendaryStrategy extends ItemUpdateStrategy {
+        public void updateSellIn(Item item) {
+        }
+
+        public void updateQuality(Item item) {
+        }
+    }
+
+    private class BackstagePassStrategy extends ItemUpdateStrategy {
+        public void updateQuality(Item item) {
+            if (item.sellIn < 0) {
+                item.quality = 0;
+            } else {
+                if (item.sellIn < 5) {
                     incrementQuality(item);
                 }
-                break;
-            case "Conjured Mana Cake":
-                decrementQuality(item);
-                decrementQuality(item);
-                if (item.sellIn < 0) {
-                    decrementQuality(item);
-                    decrementQuality(item);
+                if (item.sellIn < 10) {
+                    incrementQuality(item);
                 }
-                break;
-            default:
-                decrementQuality(item);
-                if (item.sellIn < 0) {
-                    decrementQuality(item);
-                }
-                break;
+                incrementQuality(item);
+            }
         }
+    }
+
+    private class BrieStrategy extends ItemUpdateStrategy {
+        public void updateQuality(Item item) {
+            incrementQuality(item);
+            if (item.sellIn < 0) {
+                incrementQuality(item);
+            }
+        }
+    }
+
+    private class ConjuredStrategy extends ItemUpdateStrategy {
+        public void updateQuality(Item item) {
+            decrementQuality(item);
+            decrementQuality(item);
+            if (item.sellIn < 0) {
+                decrementQuality(item);
+                decrementQuality(item);
+            }
+        }
+    }
+
+    private class ItemUpdateStrategy {
+        public void updateSellIn(Item item) {
+            decrementSellIn(item);
+        }
+
+        public void updateQuality(Item item) {
+            decrementQuality(item);
+            if (item.sellIn < 0) {
+                decrementQuality(item);
+            }
+        }
+    }
+
+    private void decrementSellIn(Item item) {
+        item.sellIn -= 1;
     }
 
     private void incrementQuality(Item item) {
