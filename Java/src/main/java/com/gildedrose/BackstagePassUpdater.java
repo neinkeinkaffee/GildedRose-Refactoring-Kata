bc67e27c1f594d09dabe6ab43527b6678542f617
Gesa Stupperich <gesa.stupperich@gmail.com>
2022-01-13 08:52:12 +0000

    Extract method

--- Java/src/main/java/com/gildedrose/BackstagePassUpdater.java
+++ Java/src/main/java/com/gildedrose/BackstagePassUpdater.java
@@ -3,15 +3,23 @@ package com.gildedrose;
 class BackstagePassUpdater extends ItemUpdater {
     public void updateQuality(Item item) {
         if (item.sellIn < 0) {
-            item.quality = 0;
+            setQualityToZeroAfterConcert(item);
         } else {
-            if (item.sellIn < 5) {
-                super.incrementQuality(item);
-            }
-            if (item.sellIn < 10) {
-                super.incrementQuality(item);
-            }
+            increaseQualityBeforeConcert(item);
+        }
+    }
+
+    private void setQualityToZeroAfterConcert(Item item) {
+        item.quality = 0;
+    }
+
+    private void increaseQualityBeforeConcert(Item item) {
+        if (item.sellIn < 5) {
             super.incrementQuality(item);
         }
+        if (item.sellIn < 10) {
+            super.incrementQuality(item);
+        }
+        super.incrementQuality(item);
     }
 }
